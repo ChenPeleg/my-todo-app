206562742fe3084552acf054c57043142fee9ffa
Chen Peleg <chenpeleg@gmail.com>
2025-09-29 17:29:51 +0300

    Add GetRequest component and integrate it into routing

--- src/components/GetRequest.jsx
+++ src/components/GetRequest.jsx
@@ -1,6 +1,28 @@
+import { Axios } from "axios"
+import { use, useEffect, useState } from "react"
+//https://emojihub.yurace.pro/api/categories
+const axoisClient = new Axios( )
+
 export const GetRequest = () => {
+    const [searchString, setSearchString] = useState('face')
+  
+    const [searchResults, setSearchResult] = useState([])
+    useEffect (()=>{
+        axoisClient.get(`https://emojihub.yurace.pro/api/search?q=${searchString}`).then((result)=> {
+            // @ts-ignore
+            setSearchResult(result)
+        })
+    }, [])
+
+
 //https://http.cat/[status_code]
     return <div>
-        try get
+        <div className="p-6 text-4xl">
+
+       Search emoji
+        </div>
+        <div>
+            {}
+        </div>
     </div>
 }
