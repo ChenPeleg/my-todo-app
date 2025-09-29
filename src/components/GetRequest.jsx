24efc3d9e591b5b10f9bd968242a24cf40793075
Chen Peleg <chenpeleg@gmail.com>
2025-09-29 17:54:29 +0300

    Fix axios client initialization and improve search handling in GetRequest component

--- src/components/GetRequest.jsx
+++ src/components/GetRequest.jsx
@@ -1,19 +1,26 @@
-import { Axios } from "axios"
+import axios, { Axios } from "axios"
 import { use, useEffect, useState } from "react"
 //https://emojihub.yurace.pro/api/categories
-const axoisClient = new Axios( )
+const axoisClient =   axios.create( )
 
 export const GetRequest = () => {
     const [searchString, setSearchString] = useState('face')
   
     const [searchResults, setSearchResult] = useState([])
+    const handleSearch = async () => {
+        const result =  await axoisClient.get(`https://emojihub.yurace.pro/api/search?q=${searchString}`)
+        const data = await result.data
+        // @ts-ignore
+        setSearchResult(data)
+    }
+
     useEffect (()=>{
-        axoisClient.get(`https://emojihub.yurace.pro/api/search?q=${searchString}`).then((result)=> {
-            // @ts-ignore
-            setSearchResult(result)
+        axoisClient.get(`https://emojihub.yurace.pro/api/search?q=${searchString}`).then((result)=>  result.data).then(data=> {
+         
+           setSearchResult (data) 
         })
     }, [])
-
+ 
  
     return <div>
         <div className="p-6 text-4xl">
@@ -24,9 +31,21 @@ export const GetRequest = () => {
             <input className="p-3 ring-1 ring-slate-500 rounded-2xl" value={searchString} onChange={(ev)=> {
                 setSearchString (ev.target.value)
             }}/>
+            <button onClick={()=>handleSearch()}> search </button>
         </div>
-        <div>
-            {}
+        <div className="flex flex-col items-start">
+            {searchResults.slice(0, 20).map(emoji=> {
+                return <div key={emoji.name}> 
+                <span dangerouslySetInnerHTML={{ __html : emoji.htmlCode
+                }}>
+                    
+                </span>
+                <span>
+                {emoji.name}
+
+                </span>
+                </div>
+            })}
         </div>
     </div>
 }
