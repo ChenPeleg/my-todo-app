c5831e3c1eb160bed166ef01d796ceb2828f560d
Chen Peleg <chenpeleg@gmail.com>
2025-09-30 12:24:50 +0300

    Add search input to GetImojies component for user emoji search

--- src/components/GetImojies.jsx
+++ src/components/GetImojies.jsx
@@ -5,6 +5,9 @@ const httpClient = axios.create();
 
 export const GetImojies = () => {
   const [emojiesResutls, setEmojiesResutls] = useState([]);
+  const [searchText, setSearchText] = useState('')
+
+
   useEffect(() => {
 
     httpClient
@@ -20,8 +23,16 @@ export const GetImojies = () => {
       <h1 className="text-3xl "> Get Imojies </h1>
       <div>
         <p>Emojies found</p>
+
+        <input value={searchText} onChange={(ev)=>{
+            setSearchText (ev.target.value)
+        }} className="bg-sky-100 p-3 rounded-full w-44"/> 
+
+        
+        <button>Search emojis</button>
+
         <div>
-          {emojiesResutls.map((emoji) => {
+          {emojiesResutls.slice(0,30).map((emoji) => {
             return <div key={emoji.name}>
                 <span className="text-4xl" dangerouslySetInnerHTML={{__html : emoji.htmlCode}}></span> 
                  {emoji.name}
