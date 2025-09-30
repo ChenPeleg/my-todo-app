d493159fbc9c480a434b947e5fa9f9d8ad3d03b4
Chen Peleg <chenpeleg@gmail.com>
2025-09-30 12:14:37 +0300

    Refactor GetImojies component to manage emoji state and fetch data from API

--- src/components/GetImojies.jsx
+++ src/components/GetImojies.jsx
@@ -1,13 +1,34 @@
-import axios from "axios"
-import { useEffect } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const httpClient =  axios.create()
+const httpClient = axios.create();
 
-export const GetImojies = ()=> {
-    useEffect (()=> {
-        console.log("get me google")
+export const GetImojies = () => {
+  const [emojiesResutls, setEmojiesResutls] = useState([]);
+  useEffect(() => {
 
-    }, [])
- 
-    return <div> Get Imojies</div>
-}
+    httpClient
+      .get("https://emojihub.yurace.pro/api/search?q=a")
+      .then((result) => {
+        setEmojiesResutls(result.data);
+      });
+
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl "> Get Imojies </h1>
+      <div>
+        <p>Emojies found</p>
+        <div>
+          {emojiesResutls.map((emoji) => {
+            return <div key={emoji.name}>
+                <span className="text-4xl" dangerouslySetInnerHTML={{__html : emoji.htmlCode}}></span> 
+                 {emoji.name}
+                 </div>;
+          })} 
+        </div>
+      </div>
+    </div>
+  );
+};
