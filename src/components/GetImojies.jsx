053500bb4dae34c0cc155b2342959cfe5fef6701
Chen Peleg <chenpeleg@gmail.com>
2025-09-30 12:39:13 +0300

    Refactor GetImojies component to improve emoji search functionality and clean up CSS file

--- src/components/GetImojies.jsx
+++ src/components/GetImojies.jsx
@@ -5,17 +5,18 @@ const httpClient = axios.create();
 
 export const GetImojies = () => {
   const [emojiesResutls, setEmojiesResutls] = useState([]);
-  const [searchText, setSearchText] = useState('')
+  const [searchText, setSearchText] = useState("");
 
-
-  useEffect(() => {
-
+  const searchImojies = () => {
     httpClient
-      .get("https://emojihub.yurace.pro/api/search?q=a")
+      .get("https://emojihub.yurace.pro/api/search?q=" + searchText)
       .then((result) => {
         setEmojiesResutls(result.data);
       });
+  };
 
+  useEffect(() => {
+    searchImojies();
   }, []);
 
   return (
@@ -24,20 +25,30 @@ export const GetImojies = () => {
       <div>
         <p>Emojies found</p>
 
-        <input value={searchText} onChange={(ev)=>{
-            setSearchText (ev.target.value)
-        }} className="bg-sky-100 p-3 rounded-full w-44"/> 
+        <input
+          value={searchText}
+          onChange={(ev) => {
+            setSearchText(ev.target.value);
+          }}
+          className="bg-sky-100 p-3 rounded-full w-44"
+        />
 
-        
-        <button>Search emojis</button>
+        <button className="bg-amber-400 p-2 rounded-4xl" onClick={()=> {
+            searchImojies()
+        }}>Search emojis</button>
 
         <div>
-          {emojiesResutls.slice(0,30).map((emoji) => {
-            return <div key={emoji.name}>
-                <span className="text-4xl" dangerouslySetInnerHTML={{__html : emoji.htmlCode}}></span> 
-                 {emoji.name}
-                 </div>;
-          })} 
+          {emojiesResutls.slice(0, 30).map((emoji) => {
+            return (
+              <div key={emoji.name}>
+                <span
+                  className="text-4xl"
+                  dangerouslySetInnerHTML={{ __html: emoji.htmlCode }}
+                ></span>
+                {emoji.name}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
