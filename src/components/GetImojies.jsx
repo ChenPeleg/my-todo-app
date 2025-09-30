78a039e6bbebdf474554c6740d8a47982f5f376d
Chen Peleg <chenpeleg@gmail.com>
2025-09-30 11:24:40 +0300

    Add useEffect hook to GetImojies component for logging

--- src/components/GetImojies.jsx
+++ src/components/GetImojies.jsx
@@ -1,10 +1,13 @@
 import axios from "axios"
-
- 
- 
+import { useEffect } from "react"
 
 const httpClient =  axios.create()
 
 export const GetImojies = ()=> {
+    useEffect (()=> {
+        console.log("get me google")
+
+    }, [])
+ 
     return <div> Get Imojies</div>
 }
